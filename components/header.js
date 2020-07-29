726113a065e9291af9d109b60976e808c28dfd5c
Mark <mso872@gmail.com>
2020-07-29 13:08:35 -0700

    updated toDoitem styling

--- components/header.js
+++ components/header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function Header() {
     return (
         <View style={styles.header}>
--- components/todoitem.js
+++ components/todoitem.js
@@ -22,9 +22,13 @@ const styles = StyleSheet.create({
         // borderStyle: 'dashed',
         borderRadius: 10,
         flexDirection: 'row',
+        shadowColor: '#135657',
+        shadowOffset: { width: 10, height: 10 },
+        shadowOpacity: 0.2,
+        shadowRadius: 5,
     },
     itemText: {
         marginLeft: 10,
-        color: '#135657',
+        color: 'white',
     }
   });
