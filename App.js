e6009b4b7340006d44345aa9f6395c12a38a7836
Mark <mso872@gmail.com>
2020-09-08 10:12:12 -0700

    added news navigation button

--- App.js
+++ App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import 'react-native-gesture-handler';
-import { Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import "react-native-gesture-handler";
+import { Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 // screens
-import Home from './screens/Home';
-import News from './screens/News';
-// import Newspage from './components/Newspage';
+import Home from "./screens/Home";
+import News from "./screens/News";
 // authentication
 import firebase from "firebase";
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
-// Set the app home screen using initialRouteName at line 87
 
 function HomeScreen({ navigation }) {
   return (
-    // <Button
-    //   title="Go to News"
-    //   onPress={() => navigation.navigate('News')}
-    // />
-    <Home />
+    <>
+      <Button title="Go to News" onPress={() => navigation.navigate("News")} />
+      <Home />
+    </>
   );
 }
 
@@ -38,15 +35,16 @@ const Stack = createStackNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home"> 
+      <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="News" component={NewsScreen}   options={{ title: 'News' }}/>
+        <Stack.Screen
+          name="News"
+          component={NewsScreen}
+          options={{ title: "News" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
 
-
-
 export default App;
-
